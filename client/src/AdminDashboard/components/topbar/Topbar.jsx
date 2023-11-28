dda06e8bab93cbea2a6fdd5d3ead78bf36c3dc42
ArditNimanaj <arditnimanaj@gmail.com>
2023-11-28 21:25:14 +0100

    fix1

--- client/src/AdminDashboard/components/topbar/Topbar.jsx
+++ client/src/AdminDashboard/components/topbar/Topbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./topbar.css";
-import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+import { NotificationsNone, Language, Settings } from "@mui/icons-material";
 import { useContext } from "react";
 import { UserContext } from "../../../UserContext";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
