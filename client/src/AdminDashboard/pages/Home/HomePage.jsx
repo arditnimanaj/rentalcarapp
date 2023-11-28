76826aeaab929656197a0769968757537011d54b
ArditNimanaj <arditnimanaj@gmail.com>
2023-11-28 21:05:27 +0100

    fix8

---
+++ client/src/AdminDashboard/pages/Home/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./home.css";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+
+export default function HomePage() {
+  return (
+    <div className="home">
+      <FeaturedInfo />
+    </div>
+  );
+}
